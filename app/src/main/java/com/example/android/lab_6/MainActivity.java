4734968771b3dd8412a58b8e588147427a76d06f
Jake <jparker22@wisc.edu>
2020-03-08 17:57:06 -0500

    Finished milestone 2 - does not get actual current location for some reason

--- app/src/main/java/com/example/android/lab_6/MainActivity.java
+++ app/src/main/java/com/example/android/lab_6/MainActivity.java
@@ -1,20 +1,33 @@
 package com.example.android.lab_6;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends FragmentActivity {
-    
+
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 12;
     private final LatLng mDestinationLatLng = new LatLng(43.0749568, -89.4048774);
     private GoogleMap googleMap;
+    private FusedLocationProviderClient mFusedLocationProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +42,47 @@ public class MainActivity extends FragmentActivity {
                 googleMap.addMarker(new MarkerOptions()
                         .position(mDestinationLatLng)
                         .title("Destination"));
+                displayMyLocation();
             }
         });
+
+        mFusedLocationProvider = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+    private void displayMyLocation() {
+        int permission = ActivityCompat.checkSelfPermission(this.getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            mFusedLocationProvider.getLastLocation().addOnCompleteListener(
+                    new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    Location mLastKnownLocation = task.getResult();
+                    if (task.isSuccessful() && mLastKnownLocation != null) {
+                        LatLng myLoc = new LatLng(mLastKnownLocation.getLatitude(),
+                                                  mLastKnownLocation.getLongitude());
+                        googleMap.addPolyline(new PolylineOptions().add(
+                                myLoc,
+                                mDestinationLatLng));
+                        googleMap.addMarker(new MarkerOptions()
+                                .position(myLoc)
+                                .title("Current Location"));
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                displayMyLocation();
+        }
     }
 }
